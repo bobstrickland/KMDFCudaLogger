30aa34e4df0afffde24f562dbf8285fa890d0e0d
Bob Strickland <bob.sansmal@gmail.com>
2015-04-10 11:39:50 -0400

    Dump entire memory to binary output file

--- DumpClient/DumpClient.c
+++ DumpClient/DumpClient.c
@@ -42,69 +42,142 @@ unsigned long HexStringToUInt(char * hexstring)
 
 
 
-int main(int argc, _TCHAR* argv[]) {
+int main(int argc, const char * argv[]) {
 #define IOCTL_CUSTOM_CODE CTL_CODE(FILE_DEVICE_UNKNOWN, 0, METHOD_OUT_DIRECT, FILE_ANY_ACCESS)
 	HANDLE hControlDevice;
-	PSHARED_MEMORY_STRUCT SharedMemory;
-
-	SharedMemory = (PSHARED_MEMORY_STRUCT)malloc(sizeof(PSHARED_MEMORY_STRUCT));
-
-	if (argc > 2 && strncmp(argv[1], "0x", 1) == 0) {
-		SharedMemory->BufferOffset = HexStringToUInt(argv[2]);
-	}
-	else if (argc > 1) {
-		SharedMemory->BufferOffset = atol(argv[1]);
-	}
-	else {
-		PCHAR offsetText;
-		offsetText = (PCHAR)malloc(sizeof(CHAR) * 64);
-		printf("Enter Buffer Offset: ");
-		gets_s(offsetText, 64);
-		SharedMemory->BufferOffset = atol(offsetText);
-	}
-	printf("Control Buffer Offset: %s\n", SharedMemory->BufferOffset);
+	NTSTATUS status = STATUS_UNHANDLED_EXCEPTION;
 
 	hControlDevice = CreateFile(TEXT("\\\\.\\DeviceMemDump"), GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
 
 	if (INVALID_HANDLE_VALUE == hControlDevice) {
 		printf("Failed to open DeviceMemDump device\n");
+		status = STATUS_INVALID_HANDLE;
 	}
 	else {
-		ULONG      BytesReturned;
-		PCHAR      ClientMemory;
-		OVERLAPPED DeviceIoOverlapped;
-		
-		ClientMemory = (PCHAR)malloc(ClientMemoryLength);
-
-		SharedMemory->ClientMemory = ClientMemory;
-
-		DeviceIoOverlapped.Offset = 0;
-		DeviceIoOverlapped.OffsetHigh = 0;
-		DeviceIoOverlapped.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
-
-		printf("\nTransmitting\n");
-		if (!DeviceIoControl(hControlDevice, IOCTL_CUSTOM_CODE, NULL, 0, SharedMemory, SharedMemoryLength, &BytesReturned, &DeviceIoOverlapped)) {
-			printf("Ioctl to DeviceMemDump device failed.\n");
+		if (argc > 2 && strncmp(argv[1], "0x", 2) == 0) {
+			printf("Hex: [%s]\n", argv[2]);
+			ULONG BufferOffset = HexStringToUInt(argv[2]);
+			status = dumpMemoryByOffset(hControlDevice, BufferOffset);
 		}
-		else {
-			printf("Ioctl to DeviceMemDump device succeeded.\nbuffer is: [");
-			if (TRUE) {
-				for (int i = 0; i < PRAMIN_LENGTH; i++) {
-					printf("%c", ClientMemory[i]);
-				}
-				printf("]\nalso: [");
-				for (int i = 0; i < PRAMIN_LENGTH; i++) {
-					printf("0x%x ", ClientMemory[i]);
-					if (i % 20 == 0) {
-						printf("]\n[");
-					}
-				}
-			}
-			printf("]\n");
+		else if (argc > 2 && strncmp(argv[1], "file", 4) == 0) {
+			printf("Everything to file\n");
+			status = dumpMemoryToFile(hControlDevice, argv[2]);
+		}
+		else if (argc > 1) {
+			printf("DEC: [%s]\n", argv[1]);
+			ULONG BufferOffset = atol(argv[1]);
+			status = dumpMemoryByOffset(hControlDevice, BufferOffset);
 		}
 		CloseHandle(hControlDevice);
 	}
 	printf("fin\n");
 	return 0;
 }
+
+NTSTATUS dumpMemoryToFile(HANDLE hControlDevice, const char * FileName) {
+
+	FILE * FilePointer;
+	printf("Opening Output file...");
+	errno_t errorCode = fopen_s(&FilePointer, FileName, "w");
+	printf("Open.\n");
+
+	PSHARED_MEMORY_STRUCT SharedMemory;
+
+	ULONG      BytesReturned;
+	PCHAR      ClientMemory;
+	OVERLAPPED DeviceIoOverlapped;
+	NTSTATUS status = STATUS_UNHANDLED_EXCEPTION;
+
+	SharedMemory = (PSHARED_MEMORY_STRUCT)malloc(sizeof(PSHARED_MEMORY_STRUCT));
+	ClientMemory = (PCHAR)malloc(ClientMemoryLength);
+
+	SharedMemory->ClientMemory = ClientMemory;
+
+	DeviceIoOverlapped.Offset = 0;
+	DeviceIoOverlapped.OffsetHigh = 0;
+	DeviceIoOverlapped.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+
+	printf("Writing pages 0-%d", PRAMIN_PAGES);
+	for (ULONG BufferOffset = 0; BufferOffset < PRAMIN_PAGES; BufferOffset++) {
+		SharedMemory->BufferOffset = BufferOffset*PRAMIN_LENGTH;
+
+		status = readMemoryByOffset(hControlDevice, SharedMemory);
+
+		if (NT_SUCCESS(status)) {
+			fwrite(ClientMemory, PRAMIN_LENGTH, 1, FilePointer);
+			printf(".");
+		}
+		else {
+			printf("Call failed. [0x%lx]\n", status);
+			BufferOffset = 1000;
+		}
+	}
+	printf("finished writing.\nClosing...");
+	fclose(FilePointer);
+	printf("Closed.\nFinished.\n");
+	return status;
+}
+
+NTSTATUS dumpMemoryByOffset(HANDLE hControlDevice, ULONG BufferOffset) {
+	PSHARED_MEMORY_STRUCT SharedMemory;
+
+	ULONG      BytesReturned;
+	PCHAR      ClientMemory;
+	OVERLAPPED DeviceIoOverlapped;
+	NTSTATUS status = STATUS_UNHANDLED_EXCEPTION;
+
+	SharedMemory = (PSHARED_MEMORY_STRUCT)malloc(sizeof(PSHARED_MEMORY_STRUCT));
+	ClientMemory = (PCHAR)malloc(ClientMemoryLength);
+
+	SharedMemory->ClientMemory = ClientMemory;
+	SharedMemory->BufferOffset = BufferOffset;
+
+	DeviceIoOverlapped.Offset = 0;
+	DeviceIoOverlapped.OffsetHigh = 0;
+	DeviceIoOverlapped.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+
+	status = readMemoryByOffset(hControlDevice, SharedMemory);
+	if (NT_SUCCESS(status)) {
+		for (ULONG i = 0; i < PRAMIN_LENGTH; i++) {
+			printf("%c", ClientMemory[i]);
+			if ((i + 1) % 128 == 0) {
+				printf("\n");
+			}
+		}
+		printf("\nIn HEX:\n[");
+		for (ULONG i = 0; i < PRAMIN_LENGTH; i++) {
+			printf("0x%x ", ClientMemory[i]);
+			if ((i + 1) % 20 == 0) {
+				printf("]\n[");
+			}
+		}
+		printf("]\n");
+	}
+	else {
+		printf("Call failed. [0x%lx]\n", status);
+	}
+	return status;
+}
+
+NTSTATUS readMemoryByOffset(HANDLE hControlDevice, PSHARED_MEMORY_STRUCT SharedMemory) {
+
+	ULONG      BytesReturned;
+	OVERLAPPED DeviceIoOverlapped;
+	NTSTATUS status = STATUS_UNHANDLED_EXCEPTION;
+
+	memset(SharedMemory->ClientMemory, 0, ClientMemoryLength);
+
+	DeviceIoOverlapped.Offset = 0;
+	DeviceIoOverlapped.OffsetHigh = 0;
+	DeviceIoOverlapped.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+
+	if (!DeviceIoControl(hControlDevice, IOCTL_CUSTOM_CODE, NULL, 0, SharedMemory, SharedMemoryLength, &BytesReturned, &DeviceIoOverlapped)) {
+		status = STATUS_IO_DEVICE_ERROR;
+	}
+	else {
+		status = STATUS_SUCCESS;
+	}
+	return status;
+}
+
 
--- DumpClient/stdafx.h
+++ DumpClient/stdafx.h
@@ -9,12 +9,17 @@
 
 #include <stdio.h>
 #include <tchar.h>
+#include <ntstatus.h>
 
 typedef unsigned long ULONG;
 typedef unsigned short USHORT;
 typedef void *  PVOID;
 
 
+#define VIDEO_MEMORY_SIZE 1024000000
+#define PRAMIN_LENGTH 0x100000
+#define PRAMIN_PAGES (VIDEO_MEMORY_SIZE/PRAMIN_LENGTH)
+#define NT_SUCCESS(Status) (((NTSTATUS)(Status)) >= 0)
 
 typedef struct _SHARED_MEMORY_STRUCT
 {
@@ -22,7 +27,11 @@ typedef struct _SHARED_MEMORY_STRUCT
 	PCHAR ClientMemory;
 } SHARED_MEMORY_STRUCT, *PSHARED_MEMORY_STRUCT;
 
-ULONG PRAMIN_LENGTH = 0x100000;
-ULONG ClientMemoryLength = (0x100001 * sizeof(CHAR));
+
+ULONG ClientMemoryLength = (0x100000 * sizeof(CHAR));
 ULONG SharedMemoryLength = sizeof(SHARED_MEMORY_STRUCT);
-// TODO: reference additional headers your program requires here
+
+
+NTSTATUS dumpMemoryByOffset(HANDLE hControlDevice, ULONG BufferOffset);
+NTSTATUS readMemoryByOffset(HANDLE hControlDevice, PSHARED_MEMORY_STRUCT SharedMemory);
+NTSTATUS dumpMemoryToFile(HANDLE hControlDevice, const char * FileName);
